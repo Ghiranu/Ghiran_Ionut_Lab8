5adb8e503de176b7a654632387c7413284004780
Ghiranu <ionutgrn@yahoo.com>
2021-11-06 16:16:41 +0200

    Add project files.

---
+++ Data/Ghiran_Ionut_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ghiran_Ionut_Lab8.Models;
+
+namespace Ghiran_Ionut_Lab8.Data
+{
+    public class Ghiran_Ionut_Lab8Context : DbContext
+    {
+        public Ghiran_Ionut_Lab8Context (DbContextOptions<Ghiran_Ionut_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ghiran_Ionut_Lab8.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ghiran_Ionut_Lab8.Data;
+using Ghiran_Ionut_Lab8.Models;
+
+namespace Ghiran_Ionut_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ghiran_Ionut_Lab8.Data.Ghiran_Ionut_Lab8Context _context;
+
+        public IndexModel(Ghiran_Ionut_Lab8.Data.Ghiran_Ionut_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher).ToListAsync();
+        }
+    }
+}
